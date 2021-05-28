e03eaec2f82724f816c8e84dbaa70c5b751ec028
mitochon9-macbook <79145642+mitochon9@users.noreply.github.com>
2021-05-29 07:39:08 +0900

    ✨ ヘッダーの追加

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { Main } from "../components/Main";
 import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Header />
       <Main page="index" />
       <Footer />
     </div>
