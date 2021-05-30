787ec383806f6508efc02dc29bea9840c2898efd
mitochon9-macbook <79145642+mitochon9@users.noreply.github.com>
2021-05-30 16:17:39 +0900

    import修正

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
+import { useEffect, useState } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
