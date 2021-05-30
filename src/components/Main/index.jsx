60d2b097fd0bdb46f63a7d76f2d099235664c0c3
mitochon9-macbook <79145642+mitochon9@users.noreply.github.com>
2021-05-30 09:41:46 +0900

    🔥 コンポーネントファイルのリファクタリング

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { Headline } from "./Headline";
-import { Links } from "./Links";
+import { Headline } from "../Headline";
+import { Links } from "../Links";
 import styles from "./Main.module.css";
 
 export function Main(props) {
